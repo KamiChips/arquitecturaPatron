580f515bae105e2a2e4e470f5bf70aba60d7a6bc
KamiChips <chimomela.santos@gmail.com>
2025-02-24 11:02:07 -0800

    Patron de diseno Adapter

---
+++ Adapter/Adapter/Adaptador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    //esta clase hace que Adaptar sea compatible con Target
+    internal class Adaptador : ITarget
+    {
+        private readonly Adaptar _adaptar;
+        public string getRequest()
+        {
+            return $"Adaptado: {_adaptar.getEspeciRequest()}";
+        }
+    }
+}
---
+++ Adapter/Adapter/ITarget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    //interfaz que el cliente espera utilizar
+    internal interface ITarget
+    {
+        string getRequest();
+    }
+}
