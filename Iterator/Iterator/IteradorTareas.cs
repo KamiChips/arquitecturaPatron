a840e8c08d86a873b6052d1b62121d55b8783104
KamiChips <chimomela.santos@gmail.com>
2025-02-24 20:05:33 -0800

    Ultimo yippeee

---
+++ Iterator/Iterator/IteradorTareas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    //iterador para la coleccion de tareas
+    internal class IteradorTareas : Iterator
+    {
+        private coleccionTareas _coleccion;
+        private int _poiscion = -1;
+
+        public IteradorTareas(coleccionTareas coleccion)
+        {
+            _coleccion = coleccion;
+        }
+        //retorna la tarea actual
+        public override object Current()
+        {
+            return _coleccion.GetTareas()[_poiscion];
+        }
+        //reiniciar el iterador
+        public override void Reset() { 
+            _poiscion = -1;
+        }
+        //avanza a la siguienete tarea
+        public override bool MoveNext()
+        {
+            _poiscion++;
+            return _poiscion < _coleccion.GetTareas().Count;
+        }
+    }
+}
---
+++ Iterator/Iterator/coleccionTareas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    /// <summary>
+    /// hacemos una coleccion 
+    /// basicamente unaa lista que contiene
+    /// tareas pendientes
+    /// </summary>
+    internal class coleccionTareas : IteradorAgregar
+    {
+        private List<string> _tareas = new List<string>();
+        
+        //acceder a la lista de tareas
+        public List<string> GetTareas()
+        {
+            return _tareas;
+        }
+
+        //agregar tarea(s) a la lista
+        public void agregarTarea(string tarea)
+        {
+            _tareas.Add(tarea);
+        }
+
+        //crea y regresa un iterador para esta coleccion
+        public override IEnumerator GetEnumerator()
+        {
+            return new IteradorTareas(this);
+        }
+    }
+}
